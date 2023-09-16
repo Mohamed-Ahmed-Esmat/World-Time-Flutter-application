178d328c1e9466093eac847e9f9c7d1c0bce5be3
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-09-16 13:46:44 +0300

    Added the http package and edited the loading file

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,8 +11,21 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getData() async {
+    Uri url = Uri.parse("https://jsonplaceholder.typicode.com/todos/1");
+    Response response = await get(url); // Use the http alias here
+    Map data = jsonDecode(response.body);
+    print(data['title']);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Text("Loading Screen"),
     );
