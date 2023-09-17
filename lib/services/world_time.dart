16323c5409117dfd5b45407812b43588a621d362
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-09-17 12:13:19 +0300

    added a try catch block

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -11,16 +11,20 @@ class WorldTime {
   WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async {
-    Uri apiurl = Uri.parse("https://worldtimeapi.org/api/timezone/$url");
-    Response response = await get(apiurl); // Use the http alias here
-    Map data = jsonDecode(response.body);
-    // print(data);
-    var datetime = data['datetime'];
-    var offset = data['utc_offset'].substring(1, 3);
-    //print(offset);
+    try {
+      Uri apiurl = Uri.parse("https://worldtimeapi.org/api/timezone/$url");
+      Response response = await get(apiurl); // Use the http alias here
+      Map data = jsonDecode(response.body);
+      // print(data);
+      var datetime = data['datetime'];
+      var offset = data['utc_offset'].substring(1, 3);
+      //print(offset);
 
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    time = now.toString();
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      time = now.toString();
+    } catch (e) {
+      time = "cannot load data";
+    }
   }
 }
